6ff3ede429a38df19b467efa3cca3893af1445ac
Yegor Chernyshev <deus.chernyshev@gmail.com>
2023-08-18 19:53:34 +0300

    Added docstrings

--- lib/src/simple_graphql_client.dart
+++ lib/src/simple_graphql_client.dart
@@ -3,11 +3,18 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 abstract class SimpleGraphQLClient {
+  /// Базовый URL
   abstract String baseUrl;
+
+  /// HTTP заголовки
   abstract Map<String, String>? headers;
 
   Uri get _uri => Uri.parse(baseUrl);
 
+  /// Метод для отправки GraphQL запроса для получения данных
+  /// ```dart
+  /// final client = SimpleGraphQLClient();
+  /// final result = await client.query('{ person(id: 10) { name age } }');
   Future<dynamic> query({required String query}) async {
     final response = await http.post(
       _uri,
@@ -20,6 +27,11 @@ abstract class SimpleGraphQLClient {
     return jsonDecode(response.body);
   }
 
+  /// Метод для отправки GraphQL запроса для изменения данных
+  /// ```dart
+  /// final client = SimpleGraphQLClient();
+  /// final result = await client.mutation('{ createSong { addSong(title: "Город под подошвой", performer: "OXXXYMIRON") { id title performer } } }');
+  /// ```
   Future<dynamic> mutation({required String query}) async {
     final response = await http.post(
       _uri,
